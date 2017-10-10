768451a5a998025630a8407c89c2ad3925588b3b
Howard Cheung <31749983+cheungh@users.noreply.github.com>
2017-10-09 23:25:00 -0700

    add Iterate option

--- algorithm/Fibonacci.php
+++ algorithm/Fibonacci.php
@@ -14,3 +14,15 @@ function Fibonacci ($n)
 
 echo Fibonacci(6);
 
+function fibonacciIterate(int $n): int {
+    
+    $cached = array(1,1);
+    for ($i = 2; $i <= $n; $i++) {
+        $a = $i - 1;
+        $b = $i - 2;
+        $cached[$i] = $cached[$a] + $cached[$b];
+    }
+
+    return $cached[$n];
+}
+echo fibonacciIterate(6);
