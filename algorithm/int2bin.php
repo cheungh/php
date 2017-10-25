a9abd0ca538c6a9b5a4032746185f71d828305ef
Howard Cheung <31749983+cheungh@users.noreply.github.com>
2017-10-25 10:41:53 -0700

    added function to convert integer to any base

--- algorithm/int2bin.php
+++ algorithm/int2bin.php
@@ -13,16 +13,6 @@ function covertInt2BinaryDebug(int $a) {
     return $return;
 }
 
-
-function covertInt2BinaryMore($a) {
-    if ($a == 0) {
-        return;
-    }
-    $reminder = $a % 2;
-    $return = covertInt2Binary(floor($a / 2)) . "$reminder";
-    return $return;
-}
-
 function covertInt2Binary($x) {
     if ($x == 0) {
 	return;
@@ -32,11 +22,41 @@ function covertInt2Binary($x) {
     
 }
 
+// any base conversation
+// defaulted to 2
+/*
+* example of Int(9)
+* floor 9 / 2 is 4
+  reminder is 1 
+  pass 4 to recursive call 
+  floor 4 is 2 ; reminder is 0
+  pass 2 to recursive call 
+  floor 2 is 1 ; reminder is 0
+  pass 1 to recursive call 
+  floor 1 is 0 ; reminder is 1
+  return 1001
+*/
+function covertInt2NumberBase($x, $base = 2) {
+    // recursive base to return
+    if ($x == 0) {
+        return;
+    }
+    // get the floor of a division
+    $floor = floor($x / $base);
+    // get the reminder number
+    $reminder = ($x % $base);
+    // resursive call
+    return covertInt2Binary($floor, $base) . $reminder;
+    // can be written in one line
+    // return covertInt2NumberBase(floor($x / $base)) . ($x % $base);
+
+}
+
 echo "<pre>";
-echo covertInt2Binary(9) . " for 9\n";
-echo covertInt2Binary(16) . " for 16\n";
-echo covertInt2Binary(2) . " for 2\n";
-echo covertInt2Binary(1) . " for 1\n";
-echo covertInt2Binary(5) . " for 5\n";
-echo covertInt2Binary(8) . " for 8\n";
+echo covertInt2NumberBase(9) . " for 9\n";
+echo covertInt2NumberBase(16) . " for 16\n";
+echo covertInt2NumberBase(2) . " for 2\n";
+echo covertInt2NumberBase(1) . " for 1\n";
+echo covertInt2NumberBase(5) . " for 5\n";
+echo covertInt2NumberBase(8) . " for 8\n";
 
